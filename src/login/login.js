036843f06c9c044bc86d2683b60c525df2d7fb48
Renan Tonon <84050603+RenanTonon@users.noreply.github.com>
2024-06-09 21:58:23 +0000

    login

--- src/login/login.js
+++ src/login/login.js
@@ -1,11 +1,26 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    enviaDados();
-})
+var users= [
+    {login: "Renan" , senha: "12354"},
+    {login: "Willian" , senha: "54312"}
+]
 
-function enviaDados(e){
+function enviaDados(e) {
     e.preventDefault();
     const form = document.querySelector("#form-login");
     const formData = new FormData(form);
-    console.log(formData.get("login"));
-    console.log(formData.get("senha")); 
+
+    const username = formData.get("login");
+    const senha = formData.get("senha");
+
+  for(var i = 0; i < users.length;i++){
+    if(users[i].login == username){
+        if(users[i].senha == senha){
+            console.log("Login correto");
+        }
+        else{
+            console.log("Login Incorreto");
+        }
+    }
+  }
 }
+
+document.querySelector("#form-login").addEventListener("submit", enviaDados);
