9289926cd45d6d73696f9677a9e4b6c085096f1b
Renan Tonon <84050603+RenanTonon@users.noreply.github.com>
2024-06-09 19:14:44 +0000

    SEI LÁ ACHO QUE É O CAMINHO

--- src/login/login.js
+++ src/login/login.js
@@ -1,3 +1,11 @@
-function validandoAcesso(){
-    var login = document.querySelector
+document.addEventListener('DOMContentLoaded', ()=>{
+    enviaDados();
+})
+
+function enviaDados(e){
+    e.preventDefault();
+    const form = document.querySelector("#form-login");
+    const formData = new FormData(form);
+    console.log(formData.get("login"));
+    console.log(formData.get("senha")); 
 }
